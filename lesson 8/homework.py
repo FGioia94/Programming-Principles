4c64ba9e1e306005441370bcff29ebcf8b567cab
FGioia94 <info@francescogioia.it>
2025-03-30 01:25:07 +0100

    added pokemon homework

--- lesson 8/homework.py
+++ lesson 8/homework.py
@@ -16,8 +16,45 @@ e falli attaccare l'un l'altro, quante volte desideri.
 Dopo il combattimento, stampa la Salute
 di entrambi, per vedere se è coerente con gli attacchi.
 """
-
+import random
+import time
 class Pokemon:
     def __init__(self, name):
         self.name = name
-        self.
+        self.health = random.randint(90, 130)
+        self.strength = random.randint(6, 10)
+    
+    def attack(self, target):
+        multiplier = random.randint(1,4)
+        if multiplier == 4:
+            print("CRITICAL HIT \n")
+        damage = self.strength * multiplier
+        target.health -= damage
+        print(f"{self.name} deals {damage} damage to {target.name}")
+
+def main():
+    starters = ["Charmander", "Squirtle", "Bulbasaur"]
+    pokemon_1 = Pokemon(random.choice(starters))
+    starters.remove(pokemon_1.name)
+    pokemon_2 = Pokemon(random.choice(starters))
+
+    print(f"{pokemon_1.name} VS {pokemon_2.name}\n")
+    
+    counter = 0
+    while not pokemon_1.health <= 0 and not pokemon_2.health <= 0:
+        attacker = pokemon_1 if counter%2 == 0 else pokemon_2
+        
+        defender = [p for p in [pokemon_1, pokemon_2] if p != attacker][0]
+        if not counter == 0:
+            print(f"{defender.name} has {defender.health} HP left\n")
+        counter += 1
+        print(f"{attacker.name}'s turn\n")
+        time.sleep(2)
+        print(f"{attacker.name} is attacking {defender.name}\n")
+        time.sleep(2)
+        attacker.attack(defender)
+        time.sleep(4)
+
+    print(f"{defender.name} has 0 HP left, {attacker.name} wins!\n")
+
+main()
